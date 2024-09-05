c8e02d88997276ccf7c04aaff1f864998dc253cb
Ken <kenlane33@gmail.com>
2024-09-05 12:15:15 -0400

    Better [SyncVar] close

--- Tutorials/Assets/Scripts/PlayerHealth.cs
+++ Tutorials/Assets/Scripts/PlayerHealth.cs
@@ -1,25 +1,31 @@
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour {
 
-  [SyncVar(CustomName = "hp"   )] public int health = 100;
-  [SyncVar(MinSyncInterval=0.5f)] public int legHealth = 100;
+public class PlayerHealth : SyncedBehaviour {
+
+  [SyncVar(CustomName = "hp", OnChangedCallback = nameof(OnHealthChanged) )] 
+  public int health = 100;
+
+  [SyncVar(updateInterval=0.5f)] public int legHealth = 100;
   [SyncVar] public int torsoHealth = 100;
   [SyncVar] public int headHealth = 100;
 
   private void OnHealthChanged(int newValue) {
     int newHealth = newValue;
     Debug.Log($"[SyncVar] Player health changed to {newValue}");
-    if (newHealth <= 0) {
+    if        (newHealth <= 0) {
       Die();
-    }
-    else if (newHealth < 20) {
+    } else if (newHealth < 70) {
       ShowLowHealthWarning();
     }
   }
 
-  private void Die() { /* ... */ }
-  private void ShowLowHealthWarning() { /* ... */ }
+  private void Die() { 
+    Debug.Log($"<color=red>Player died</color> health:{health}");
+  }
+  private void ShowLowHealthWarning() {
+    Debug.Log($"<color=yellow>Player health is low</color> health:{health}");
+  }
 
   void OnGUI() {
     GUI.backgroundColor = new Color(0f, 0f, 0f, 0.2f);
@@ -31,7 +37,7 @@ public class PlayerHealth : MonoBehaviour {
     GUI.Label(new Rect(10, 70, 100, 20), $"Head:   {headHealth.ToString("F1")}");
   }
   void CalcHealth() {
-    health = (legHealth + torsoHealth + headHealth) / 3;
+    // health = (legHealth + torsoHealth + headHealth) / 3;
   }
   void Update() {
     // each key loses healt on a body part, l,t,h
