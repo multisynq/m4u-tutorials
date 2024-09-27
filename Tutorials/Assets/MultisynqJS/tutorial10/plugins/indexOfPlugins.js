bdcb315cdc72d78bf8d4263bdbe0cbd49d1239b8
Ken <kenlane33@gmail.com>
2024-09-27 19:47:17 -0400

    Manually made indexOfPlugins.js

--- Tutorials/Assets/MultisynqJS/tutorial10/plugins/indexOfPlugins.js
+++ Tutorials/Assets/MultisynqJS/tutorial10/plugins/indexOfPlugins.js
@@ -1,16 +1,12 @@
-// DO NOT EDIT
 // DO NOT EDIT, please.  =]
 // This file is generater by the M4U build process.
 // 
 import { GameModelRoot } from '@croquet/game-models';
 import { GameViewRoot } from '@croquet/unity-bridge';
 // ########
-// {{imports}}
-// import {{vars}} from '{{file}}'
-// {{/imports}}
-import { SynqClones_Mgr_Model, SynqClones_Mgr_View } from './SynqClones_Mgr_Model'
-import { SynqCommand_Mgr_Model, SynqCommand_Mgr_View } from './SynqCommand_Mgr_Model'
-import { SynqVar_Mgr_Model, SynqVar_Mgr_View } from './SynqVar_Mgr_Model'
+import { SynqClones_Mgr_Model, SynqClones_Mgr_View } from './SynqClones_Mgr'
+import { SynqCommand_Mgr_Model } from './SynqCommand_Mgr'
+import { SynqVar_Mgr_Model, SynqVar_Mgr_View } from './SynqVar_Mgr'
 // ########
 
 //========== |||||||||||||||| =================================================================
@@ -19,10 +15,7 @@ export class PluginsModelRoot extends GameModelRoot {
   init(options) {
     // @ts-ignore
     super.init(options);
-    // ########
-    // {{#modelInits}}
-    // this.plugins['{{modelClass}}'] = {{modelClass}}.create({})
-    // {{/modelInits}}
+    // ########  modelInits
     this.plugins['SynqClones_Mgr_Model'] = SynqClones_Mgr_Model.create({})
     this.plugins['SynqCommand_Mgr_Model'] = SynqCommand_Mgr_Model.create({})
     this.plugins['SynqVar_Mgr_Model'] = SynqVar_Mgr_Model.create({})
@@ -38,10 +31,7 @@ export class PluginsViewRoot extends GameViewRoot {
   plugins={}
   constructor(model) {
     super(model);
-    // #########
-    // {{#viewInits}}
-    // this.plugins['{{viewClass}}'] = new {{viewClass}}(model.plugins['{{modelClass}}'])
-    // {{/viewInits}}
+    // #########  viewInits
     this.plugins['SynqVar_Mgr_View'] = new SynqVar_Mgr_View(model.plugins['SynqVar_Mgr_Model'])
     this.plugins['SynqClones_Mgr_View'] = new SynqClones_Mgr_View(model.plugins['SynqClones_Mgr_Model'])
     this.plugins['SynqCommand_Mgr_View'] = new SynqCommand_Mgr_View(model.plugins['SynqCommand_Mgr_Model'])
